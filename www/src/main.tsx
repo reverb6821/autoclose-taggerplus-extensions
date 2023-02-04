88eb5921eb3032cd2f100f3289a43e9ba9fb3b6b
reverb6821 <giuseppegigliottidev@gmail.com>
2023-02-04 21:06:12 +0100

    setted eslint, tailwind and redux

--- www/src/main.tsx
+++ www/src/main.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import store from './store/store'
+
 import App from './App'
 import './index.css'
 
-// function onWindowClose() {
-//   Neutralino.app.exit();
-// }
-
-// Neutralino.init();
-// Neutralino.events.on("windowClose", onWindowClose);
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 )
